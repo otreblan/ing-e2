a7a014c5b2c9cb8c720b0b252565798aa7649a22
otreblan <otreblain@gmail.com>
2021-07-26 14:53:18 -0500

    Add state and age

--- src/main/java/ing/e2/Persona.java
+++ src/main/java/ing/e2/Persona.java
@@ -2,11 +2,43 @@ package ing.e2;
 
 public class Persona
 {
-    enum State
+    public enum State
     {
         SIN_VACUNA,
         VACUNA_PARCIAL,
         VACUNA_COMPLETA
+    }
+
+    private State state;
+    private int   age;
+
+    public Persona(int age) {
+        this.state = State.SIN_VACUNA;
+        this.age   = age;
+    }
+
+    public void vax() {
+        switch(state) {
+            case SIN_VACUNA:
+                state = State.VACUNA_PARCIAL;
+                break;
+
+            case VACUNA_PARCIAL:
+                state = State.VACUNA_COMPLETA;
+                break;
+
+            case VACUNA_COMPLETA:
+                state = State.VACUNA_COMPLETA;
+                break;
+        }
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public int getAge() {
+        return age;
     }
 }
 
