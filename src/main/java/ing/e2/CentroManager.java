660c2873e5f03095cd12eff158988bf1b101dac3
otreblan <otreblain@gmail.com>
2021-07-26 14:44:40 -0500

    Número de centros

--- src/main/java/ing/e2/CentroManager.java
+++ src/main/java/ing/e2/CentroManager.java
@@ -1,7 +1,20 @@
 package ing.e2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CentroManager
 {
+    private List<Centro> centros;
+
+    public CentroManager() {
+        this.centros = new ArrayList<>();
+    }
+
+    /// Obtiene el número de centros.
+    public int getCentroSize() {
+        return centros.size();
+    }
 }
 
 // vim: set et:
