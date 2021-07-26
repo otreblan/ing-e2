c4bc49f2531b534d8782e081e85066b366a9ad64
otreblan <otreblain@gmail.com>
2021-07-26 15:11:49 -0500

    Add login()

--- src/main/java/ing/e2/App.java
+++ src/main/java/ing/e2/App.java
@@ -1,14 +1,42 @@
 package ing.e2;
 
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class App
 {
     static final Logger logger = Logger.getLogger(App.class.getName());
+    static Scanner scanner = new Scanner(System.in);
+
+    private static boolean goodPassword(String usuario, String password) {
+        if(usuario.length() != password.length()) {
+            return false;
+        }
+
+        for(int i = 0; i < usuario.length(); i++) {
+            if(usuario.charAt(i) != password.charAt(usuario.length()-1-i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean login() {
+        logger.info("Usuario: ");
+        String usuario = scanner.nextLine();
+
+        logger.info("Contraseña: ");
+        String password = scanner.nextLine();
+
+        return goodPassword(usuario, password);
+    }
 
     public static void main( String[] args )
     {
-        logger.info("Hello world");
+        if(!login()) {
+            System.exit(1);
+        }
     }
 }
 
