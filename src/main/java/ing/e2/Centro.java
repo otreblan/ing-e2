90148ea825662be30484a380256c1fbd850eae3c
otreblan <otreblain@gmail.com>
2021-07-26 14:46:30 -0500

    Add Centro constructor

--- src/main/java/ing/e2/Centro.java
+++ src/main/java/ing/e2/Centro.java
@@ -1,7 +1,15 @@
 package ing.e2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Centro
 {
+    private List<Persona> personas;
+
+    public Centro() {
+        this.personas = new ArrayList<>();
+    }
 }
 
 // vim: set et:
